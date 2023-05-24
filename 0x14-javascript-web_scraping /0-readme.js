bc3721ac6d6ec2371814ad57774d76372b2f346d
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2023-05-24 21:31:17 +0100

    Create 0-readme.js

---
+++ 0x14-javascript-web_scraping /0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const argms = process.argv[2];
+fs.readFile(argms, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
